9d5f4c4bba030de0294cb4e8909da04c7179183a
Evan Morrison <evanrmorrison@gmail.com>
2017-06-17 06:12:54 -0700

    add routing for views; styling and ui improvements

--- src/app.config.js
+++ src/app.config.js
@@ -6,8 +6,37 @@
 
       $urlRouterProvider.otherwise('/');
 
-        
+      $stateProvider
+      // .state('home', {
+      //   url: '/',
+      //   component: 'cases'
+      // })
+      .state('cases', {
+        url: '/cases',
+        component: 'cases',
+        resolve: {
+          cases: function(caseService) {
+            return caseService.LoadAllCases();
+          }
+        }
+      })
+      .state('cases.case', {
+        url: '/{fileId}',
+        component: 'case',
+        resolve: {
+          case: function(cases, $stateParams) {
+            return cases.find(function(c){
+              return c.file === $stateParams.fileId
+            })
+            }
+          }
+      })
 
     }])
+
+    .config(function($mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+        .primaryPalette('light-blue')
+    })
 
 })();
---
+++ src/services/Case.Service.js
@@ -0,0 +1,103 @@
+(function() {
+  'use strict';
+
+  angular.module('FCMaxApp')
+    .service('caseService', ['$q', CaseService])
+
+    function CaseService($q) {
+
+      const cases = [
+        {
+          "file": "17-34829",
+          "property": {
+            "address1": "7561 S Highland Dr.",
+            "city": "Salt Lake City",
+            "county": "Salt Lake",
+            "taxId": "02-385-3829-025"
+          },
+          "lender": {
+            "name": "Steve Choat",
+            "phone": "4358783202",
+            "address1": "224 E 800 S",
+            "city": "Farmington",
+            "state": "UT",
+            "zip": "84032"
+          },
+          "borrower": {
+            "name": "Paul Brack",
+            "address1":"5822 W 200 S",
+            "city":"Bluffdale",
+            "state": "UT",
+            "zip": "84138",
+            "loanAmount": 150000,
+            "defaultDate": "",
+            "saleDate": ""
+          }
+        },
+        {
+          "file": "16-77810",
+          "property": {
+            "address1": "3183 S State St.",
+            "city": "Ogden",
+            "county": "Weber",
+            "taxId": "37-8280-02"
+          },
+          "lender": {
+            "name": "Diane Mulkazy",
+            "phone": "4352230958",
+            "address1": "3852 S Blackrock Dr",
+            "city": "Sandy",
+            "state": "UT",
+            "zip": "84260"
+          },
+          "borrower": {
+            "name": "Thomas Peterson",
+            "address1":"7237 S Chapita Way",
+            "city":"Pleasant Grove",
+            "state": "UT",
+            "zip": "84263",
+            "loanAmount": 688300,
+            "defaultDate": "",
+            "saleDate": ""
+          }
+        },
+        {
+          "file": "17-60612",
+          "property": {
+            "address1": "720 N Canyon View Rd.",
+            "city": "St. George",
+            "county": "Washington",
+            "taxId": "SG-BR357"
+          },
+          "lender": {
+            "name": "Kirk Brontwell",
+            "phone": "8017823385",
+            "address1": "485 E 300 S",
+            "city": "Salt Lake City",
+            "state": "UT",
+            "zip": "84111"
+          },
+          "borrower": {
+            "name": "Sherri Perkins",
+            "address1":"52 N Lakeview Blvd",
+            "city":"Cottonwood Heights",
+            "state": "UT",
+            "zip": "84102",
+            "loanAmount": 253700,
+            "defaultDate": "",
+            "saleDate": ""
+          }
+        }
+      ]
+
+      return {
+        LoadAllCases: function() {
+          
+          return $q.when(cases)
+
+        }
+      }
+
+    }
+
+})();
