fe106342ab13517a0a33a1705afa254f311eb65f
Evan Morrison <evanrmorrison@gmail.com>
2017-06-23 23:32:20 -0700

    replace hard coded data with dynamic service, add edit/save ui

--- components/caseMain/caseMain.component.js
+++ components/caseMain/caseMain.component.js
@@ -40,64 +40,17 @@
           // get the datepicker pane element, which when opened is always added at 
           // the end of <body>
           const dpickerPane = angular.element(document.querySelector('body').lastChild);
-            
-          // move the datepicker pane to be near its input container button
-          dpickerPane.css('top', pcPosition + 300 + 'px');
 
+          // move the datepicker pane to be near its input container button
+          if (dpickerPane.hasClass('md-datepicker-calendar-pane')) {
+            dpickerPane.css('top', pcPosition + 300 + 'px');
+          }
       }
       
       // data for testing
       ctrl.Counties = ["Box Elder", "Davis", "Salt Lake", "Summit", "Uintah", "Wasatch", "Washington", "Weber"]
 
-      ctrl.property = {
-        address: "13700 N Fountain Hills Boulevard, Apt 202 \nSyracuse, UT 84014",
-        county: "Davis",
-        taxId: "36-238-9984",
-        owner: "Stuart McFarlain",
-        legalDescription: "Lot 75, VISTA BONITA HEIGHTS SUBDIVISION, according to the official plat thereof on file and of record in the Office of the Recorder for Davis County, Utah."
-      }
-       
-      ctrl.lender = {
-        name: "Peter Boghashian",
-        address: "50 S Main Street\nSalt Lake City, UT 84111",
-        phone: "801-412-8382",
-        email: "pboghashian@email.net"
-      }
-
-      ctrl.borrower = {
-        name: "Jamie Lannister",
-        address: "301 South 200 East, Ste 150\nSalt Lake City, UT 84111",
-        phone: "801-558-0192",
-        email: ""
-      }
-
-      ctrl.loan = {
-        amount: "$257,842",
-        DOT: {
-                recorded: new Date(2015,03,03),
-                recString: "04-03-2015",
-                entry: "158293045",
-                origBeneficiary: "Oscar Travers",
-                origTrustee: "First American Title Insurance Company",
-                nonBorrowerTrustors: ""
-        },
-        assignments:[ 
-              {
-                recorded: new Date(2017,04,12),
-                recString: "05-12-2017",
-                entry: "172389298",
-                assignee: "Peter Boghasian"
-              }
-        ],
-        substitutions: [
-              {
-                recorded: "",
-                entry: "",
-                newTrustee: ""
-              }
-        ]
-      }
-
+      
     }
 
 
